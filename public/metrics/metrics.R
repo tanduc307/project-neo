61696f7784ebc3defd099de0b6f4af329aee593e
Duc Nguyen <46308688+tanduc307@users.noreply.github.com>
2023-06-08 14:32:46 +0700

    .

--- public/metrics/metrics.R
+++ public/metrics/metrics.R
@@ -1,8 +1,52 @@
-bibliometrix::biblioshiny()
+# bibliometrix::biblioshiny()
 library(bibliometrix)
 
 file <- "demo.bib"
 
-M <- convert2df(file = file, dbsource = "scopus", format = "bibtex")
+M <- bibliometrix::convert2df(file = file, dbsource = "scopus", format = "bibtex")
 
-write.csv(M, "DEMO.csv")
+M[1:220, ] -> N
+
+M[1:220, ] -> E
+
+write.csv(N, "OKP.csv")
+# 
+# N <- read.csv("CHECK.csv")
+# 
+# names(N)
+# 
+jabref <- read.csv("jabref.csv")
+# 
+# names(jabref)
+# 
+# # jabref
+
+N[, ] <- NA
+
+jabref$Identifier
+row.names(N) <- jabref$Identifier
+
+View(N)
+
+############
+N$AU <- jabref$Author
+
+N$AB <- jabref$Custom1
+
+N$SO <- jabref$Journal
+
+N$PP <- jabref$Pages
+
+N$TI <- jabref$Title
+
+N$DT <- "ARTICLE"
+
+N$VL <- jabref$Volume
+
+N$PY <- jabref$Year
+
+################################
+
+N$keywords.plus <- NA
+
+View(N)
